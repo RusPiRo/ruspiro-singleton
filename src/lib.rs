19441735848d03ff4ce1b88f2944108dc8b0301d
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-12-11 20:31:36 +0100

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-/*********************************************************************************************************************** 
+/***********************************************************************************************************************
  * Copyright (c) 2019 by the authors
- * 
- * Author: André Borrmann 
+ *
+ * Author: André Borrmann
  * License: Apache License 2.0
  **********************************************************************************************************************/
 #![doc(html_root_url = "https://docs.rs/ruspiro-singleton/0.3.0")]
@@ -9,36 +9,36 @@
 #![feature(asm)]
 
 //! # Singleton pattern implementation
-//! 
+//!
 //! Provide a cross core synchronisation safe singleton implementation pattern
-//! 
+//!
 //! # Example
 //! ```
 //! use ruspiro_singleton::*;
-//! 
+//!
 //! static FOO: Singleton<Foo> = Singleton::new(Foo::new(0));
-//! 
+//!
 //! struct Foo {
 //!     count: u32,
 //! }
-//! 
+//!
 //! impl Foo {
 //!     pub const fn new(initial_count: u32) -> Self {
 //!         Foo {
 //!             count: initial_count,
 //!         }
 //!     }
-//! 
+//!
 //!     pub fn count(&self) -> u32 {
 //!         self.count    
 //!     }
-//! 
+//!
 //!     pub fn add_count(&mut self, value: u32) -> u32 {
 //!         self.count += value;
 //!         self.count
 //!     }
 //! }
-//! 
+//!
 //! fn main () {
 //!     let counter = FOO.take_for( |foo| {
 //!         println!("secure access to the singleton");
@@ -48,12 +48,12 @@
 //!         // value of the closure given to this function.
 //!         c
 //!     });
-//! 
+//!
 //!     println!("successfull {}", counter);
 //! }
 //! ```
-//! 
-//! In case only immutable access to the contents of the singleton is required the ``use_for`` function 
+//!
+//! In case only immutable access to the contents of the singleton is required the ``use_for`` function
 //! can be used.
 //! ```
 //! # use ruspiro_singleton::*;
@@ -67,21 +67,21 @@
 //! #             count: initial_count,
 //! #         }
 //! #     }
-//! # 
+//! #
 //! #     pub fn count(&self) -> u32 {
 //! #         self.count    
 //! #     }
 //! # }
-//! 
+//!
 //! fn main () {
 //!     let counter = FOO.use_for( |foo| {
 //!             foo.count()
 //!         });
-//! 
+//!
 //!     println!("current counter: {}", counter);
 //! }
 //! ```
-//! 
+//!
 use core::cell::UnsafeCell;
 use ruspiro_lock::Spinlock;
 
@@ -93,15 +93,14 @@ pub struct Singleton<T: 'static> {
 
 // The Singleton need to implement Sync to ensure cross core sync compile check mechanisms
 #[doc(hidden)]
-unsafe impl<T> Sync for Singleton<T> { }
+unsafe impl<T> Sync for Singleton<T> {}
 
 #[doc(hidden)]
-unsafe impl<T> Send for Singleton<T> { }
+unsafe impl<T> Send for Singleton<T> {}
 
 impl<T: 'static> Singleton<T> {
-    
     /// Create a new singleton instance to be used in a static variable. Only ``const fn`` constructors are allowed here.
-    /// If this is not sufficient the ``Singleton`` may be further wrapped by a ``lazy_static!`` available as 
+    /// If this is not sufficient the ``Singleton`` may be further wrapped by a ``lazy_static!`` available as
     /// external crate from [crates.io](https://crates.io/crates/lazy_static)
     pub const fn new(data: T) -> Singleton<T> {
         Singleton {
@@ -112,7 +111,7 @@ impl<T: 'static> Singleton<T> {
 
     /// Take the stored singleton for whatever operation and prevent usage by other cores
     /// Safe access to the singleton mutable instance is guarantied inside the given closure.
-    /// 
+    ///
     /// # Example
     /// ```
     /// # use ruspiro_singleton::*;
@@ -124,25 +123,26 @@ impl<T: 'static> Singleton<T> {
     /// # }
     /// ```
     pub fn take_for<F, R>(&self, f: F) -> R
-        where F: FnOnce(&mut T) -> R 
+    where
+        F: FnOnce(&mut T) -> R,
     {
-            // to ensure atomic access to the singleton wrapped resource we aquire a lock before allowing to access
-            // the same. While the lock is aquired interrupts are disabled. This ensures there are now deadlocks 
-            // possible when a lock is interrupted and the handler tries to aquire the same lock
-            self.lock.aquire();
-            
-            let r = f( unsafe { &mut *self.inner.get() } );
-            
-            // after processing we can release the lock so other cores can access the singleton as well
-            // this also re-enables interrupts
-            self.lock.release();
-            r
+        // to ensure atomic access to the singleton wrapped resource we aquire a lock before allowing to access
+        // the same. While the lock is aquired interrupts are disabled. This ensures there are now deadlocks
+        // possible when a lock is interrupted and the handler tries to aquire the same lock
+        self.lock.aquire();
+
+        let r = f(unsafe { &mut *self.inner.get() });
+
+        // after processing we can release the lock so other cores can access the singleton as well
+        // this also re-enables interrupts
+        self.lock.release();
+        r
     }
 
     /// Immutable access to a singleton for a specific operation.
     /// This access does not enforce any lock nor guarantees safe atomic access to the instance. However, it is usefull
     /// in read-only access scenarios like inside interrupt handlers.
-    /// 
+    ///
     /// # Example
     /// ```
     /// # use ruspiro_singleton::*;
@@ -154,8 +154,9 @@ impl<T: 'static> Singleton<T> {
     /// # }
     /// ```
     pub fn use_for<F, R>(&self, f: F) -> R
-        where F: FnOnce(&T) -> R
+    where
+        F: FnOnce(&T) -> R,
     {
-            f( unsafe { & *self.inner.get() } )
+        f(unsafe { &*self.inner.get() })
     }
 }
